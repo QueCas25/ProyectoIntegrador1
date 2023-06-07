481a77306c68d86e0bf13308b362bd031dbb8726
Debs-02 <115590840+Debs-02@users.noreply.github.com>
2023-06-07 01:50:08 -0600

    primer pantalla

---
+++ app/src/main/java/com/example/proyectodismovjava/Inicio.java
@@ -0,0 +1,14 @@
+package com.example.proyectodismovjava;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inicio extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+
+    }
+}
