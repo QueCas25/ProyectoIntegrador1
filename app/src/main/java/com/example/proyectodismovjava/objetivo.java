f1326d10e240bf8d1ed8c79cbf27f0df112423a5
Paumlsg <118135378+Paumlsg@users.noreply.github.com>
2023-06-07 02:17:51 -0600

    cambio

---
+++ app/src/main/java/com/example/proyectodismovjava/objetivo.java
@@ -0,0 +1,14 @@
+package com.example.proyectodismovjava;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class objetivo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.objetivo);
+
+
+    }
+}
